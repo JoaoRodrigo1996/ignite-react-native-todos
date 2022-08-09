02f90bc6c7ca914acc42c48319a16dbacbe23168
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2022-08-09 10:51:06 -0300

    Todo - React Native

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,45 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+interface TaskData {
+  id: number
+  title: string
+  done: boolean
+}
+
 export function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<TaskData[]>([]);
+  const [newTask, setNewTark] = useState('')
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(state => [...state, data])
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    const updatedTask = tasks.map((task) => ({ ...task }))
+
+    const findTask = updatedTask.find(item => item.id === id)
+
+    if(!findTask){
+      return
+    }
+
+    findTask.done = !findTask.done
+    setTasks(updatedTask)
+    
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(state => state.filter(task => task.id !== id))
+
   }
 
   return (
